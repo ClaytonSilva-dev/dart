d2a66c674262417f262773f01dfe6f9b01a191a1
ClaytonSilva-dev <claytonsilva.dev@gmail.com>
2019-10-25 20:57:26 -0300

    Adicionei exercicio 2

--- exerc_sequencial.dart
+++ exerc_sequencial.dart
@@ -3,7 +3,23 @@
 
 /*
 1. Faça um Programa que mostre a mensagem "Alo mundo" na tela.
-*/
+
 void main(){
   print('Alo mundo.');
 }
+*/
+
+/*
+2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+
+import 'dart:io';
+void main(){
+  print('Digite um numero: ');
+
+  int num = int.parse(stdin.readLineSync());
+
+  print ('O numero digitado é: $num');
+}
+
+*/
