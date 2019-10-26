ab39abd65c39dccd25b1232cfb5cd671ef2923e8
ClaytonSilva-dev <claytonsilva.dev@gmail.com>
2019-10-25 21:05:25 -0300

    Adicionei exercio 3

--- exerc_sequencial.dart
+++ exerc_sequencial.dart
@@ -23,3 +23,24 @@ void main(){
 }
 
 */
+
+/* 3. Faça um Programa que peça dois números e imprima a soma.*/
+
+import 'dart:io';
+
+void main(){
+  print('Digite um numero');
+  int num1 = int.parse(stdin.readLineSync());
+  print('Digite mais um numero');
+  int num2 = int.parse(stdin.readLineSync());
+
+  int soma = num1 + num2;
+
+  print('A soma dos dois números é : $soma');
+  
+
+
+
+}
+
+
